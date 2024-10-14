Fix variance of SlateArray

slate/explicit_basis/_explicit_basis.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Any, Self, cast
+from typing import TYPE_CHECKING, Any, Never, Self, cast
 
 import numpy as np
 
@@ -18,7 +18,7 @@ class ExplicitBasis[M: BasisMetadata, DT: np.generic](WrappedBasis[M, DT]):
 
     def __init__(
         self: Self,
-        data: SlateArray[VariadicTupleBasis[Basis[M, DT], Basis[M, DT], Any, DT]],
+        data: SlateArray[DT, VariadicTupleBasis[Basis[M, DT], Basis[M, DT], Any, DT]],
     ) -> None:
         self._data = data
         super().__init__(data.basis[1])
@@ -34,8 +34,11 @@ class ExplicitBasis[M: BasisMetadata, DT: np.generic](WrappedBasis[M, DT]):
         axis: int = -1,
     ) -> np.ndarray[Any, np.dtype[DT1]]:
         transformed = np.tensordot(
-            cast(np.ndarray[Any, np.dtype[Any]], vectors),
-            self._data.raw_data.reshape(self._data.basis.shape),
+            cast(np.ndarray[Any, np.dtype[Never]], vectors),
+            cast(
+                np.ndarray[Any, np.dtype[Never]],
+                self._data.raw_data.reshape(self._data.basis.shape),
+            ),
             axes=([axis], [0]),
         )
         return np.moveaxis(transformed, -1, axis)
@@ -46,11 +49,13 @@ class ExplicitBasis[M: BasisMetadata, DT: np.generic](WrappedBasis[M, DT]):
         axis: int = -1,
     ) -> np.ndarray[Any, np.dtype[DT1]]:
         transformed = np.tensordot(
-            cast(np.ndarray[Any, np.dtype[Any]], vectors),
-            cast(
-                np.ndarray[Any, np.dtype[Any]],
-                np.linalg.inv(self._data.raw_data.reshape(self._data.basis.shape)),
+            cast(np.ndarray[Any, np.dtype[Never]], vectors),
+            np.linalg.inv(
+                cast(
+                    np.ndarray[Any, np.dtype[Never]],
+                    self._data.raw_data.reshape(self._data.basis.shape),
+                )
             ),
             axes=([axis], [0]),
         )
-        return np.moveaxis(transformed, -1, axis)
+        return np.moveaxis(cast(np.ndarray[Any, np.dtype[DT1]], transformed), -1, axis)

tests/conftest.py
@@ -26,12 +26,12 @@ def sample_data(shape: tuple[int, ...]) -> np.ndarray[Any, np.dtype[np.int64]]:
 @pytest.fixture
 def slate_array_integer(
     sample_data: np.ndarray[Any, np.dtype[np.int64]],
-) -> SlateArray[FundamentalBasis[FundamentalBasisMetadata, np.int64]]:
+) -> SlateArray[np.int64, FundamentalBasis[FundamentalBasisMetadata]]:
     return SlateArray.from_array(sample_data)
 
 
 @pytest.fixture
 def slate_array_complex(
     sample_data: np.ndarray[Any, np.dtype[np.int64]],
-) -> SlateArray[FundamentalBasis[FundamentalBasisMetadata, np.complex128]]:
+) -> SlateArray[np.complex128, FundamentalBasis[FundamentalBasisMetadata]]:
     return SlateArray.from_array(sample_data.astype(np.complex128))

tests/conversion_test.py
@@ -12,7 +12,7 @@ from slate.basis.transformed import TransformedBasis
 
 def test_transformed_basis_round_trip(
     slate_array_complex: SlateArray[
-        FundamentalBasis[FundamentalBasisMetadata, np.complex128]
+        np.complex128, FundamentalBasis[FundamentalBasisMetadata]
     ],
 ) -> None:
     basis = TransformedBasis(slate_array_complex.basis)
@@ -36,8 +36,8 @@ def test_transformed_basis_round_trip(
 
 def test_diagonal_basis_round_trip() -> None:
     full_basis = VariadicTupleBasis[
-        FundamentalBasis[FundamentalBasisMetadata, np.generic],
-        FundamentalBasis[FundamentalBasisMetadata, np.generic],
+        FundamentalBasis[FundamentalBasisMetadata],
+        FundamentalBasis[FundamentalBasisMetadata],
         None,
         np.generic,
     ]((FundamentalBasis.from_shape((10,)), FundamentalBasis.from_shape((10,))), None)

tests/variance_test.py
@@ -4,9 +4,41 @@ from typing import Any, cast
 
 import numpy as np
 
+from slate.array.array import SlateArray
 from slate.basis import Basis
+from slate.basis._basis import FundamentalBasis
 from slate.basis.metadata import BasisMetadata, FundamentalBasisMetadata
+from slate.basis.stacked._tuple_basis import TupleBasis
 
-a = cast(Basis[FundamentalBasisMetadata, np.floating[Any]], {})
-b: Basis[BasisMetadata, np.generic] = a  # type: ignore should fail
-c: Basis[BasisMetadata, np.float128] = a
+
+def basis_variance() -> None:
+    a = cast(Basis[FundamentalBasisMetadata, np.floating[Any]], {})
+    _b: Basis[BasisMetadata, np.generic] = a  # type: ignore should fail
+    _c: Basis[BasisMetadata, np.float128] = a
+
+
+def tuple_basis_variance() -> None:
+    a = cast(
+        TupleBasis[FundamentalBasisMetadata, np.floating[Any], np.floating[Any]], {}
+    )
+    _b: Basis[BasisMetadata, np.generic] = a  # type: ignore should fail
+    _c: Basis[BasisMetadata, np.float128] = a
+    _d: TupleBasis[BasisMetadata, np.generic, np.float128] = a  # type: ignore should fail
+    _d: TupleBasis[BasisMetadata, np.float128, np.float128] = a
+
+
+def fundamental_basis_variance() -> None:
+    a = cast(FundamentalBasis[FundamentalBasisMetadata], {})
+    _b: Basis[BasisMetadata, np.generic] = a
+    _c: Basis[BasisMetadata, np.float128] = a
+
+
+def slate_array_basis_variance() -> None:
+    a = cast(
+        SlateArray[np.float64, Basis[FundamentalBasisMetadata, np.float64]],
+        {},
+    )
+    _b: SlateArray[np.floating[Any], Basis[BasisMetadata, np.generic]] = a  # type: ignore should fail
+    _c: SlateArray[np.floating[Any], Basis[BasisMetadata, np.float64]] = a
+    _d: SlateArray[np.float128, Basis[BasisMetadata, np.float64]] = a  # type: ignore should fail
+    _e: SlateArray[np.generic, Basis[BasisMetadata, np.float64]] = a