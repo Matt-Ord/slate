06a4977fd625347f29c2f95e94438ae424c66979
Matthew Ord <matthew.ord1@gmail.com>
2024-10-21 10:06:59 +0000

    Fix lints

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,27 +1,12 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+project = "Slate"
+author = "Matthew Ord"
+version = "0.0.1"
 
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+extensions = ["sphinx.ext.autodoc"]
 
-project = 'Slate'
-copyright = '2024, Matthew Ord'
-author = 'Matthew Ord'
-
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
-extensions = []
-
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
 
 
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "alabaster"
+html_static_path = ["_static"]
