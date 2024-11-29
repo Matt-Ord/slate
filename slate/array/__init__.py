b54050e0a4294460d7910f9b97278119f30ba52b
Matthew Ord <matthew.ord1@gmail.com>
2024-11-29 13:40:06 +0000

    Add more array utils

--- slate/array/__init__.py
+++ slate/array/__init__.py
@@ -6,16 +6,28 @@ from slate.array._array import SlateArray
 from slate.array._conversion import with_basis
 from slate.array._transpose import transpose
 from slate.array._util import (
+    as_add_basis,
+    as_diagonal_basis,
+    as_feature_basis,
     as_flatten_basis,
+    as_index_basis,
+    as_mul_basis,
     as_outer_basis,
+    as_sub_basis,
     as_tuple_basis,
     conjugate,
 )
 
 __all__ = [
     "SlateArray",
+    "as_add_basis",
+    "as_diagonal_basis",
+    "as_feature_basis",
     "as_flatten_basis",
+    "as_index_basis",
+    "as_mul_basis",
     "as_outer_basis",
+    "as_sub_basis",
     "as_tuple_basis",
     "conjugate",
     "transpose",
