dd41a91ad9cb9c8ae55aefb1dc920931701202ab
Matthew Ord <matthew.ord1@gmail.com>
2024-12-05 09:55:09 +0000

    Add abs function

--- slate/linalg/_misc.py
+++ slate/linalg/_misc.py
@@ -4,14 +4,15 @@ from typing import TYPE_CHECKING, Any
 
 import numpy as np
 
+from slate import array as _array
 from slate import basis
-from slate.array._array import Array
-from slate.basis._diagonal import DiagonalBasis
-from slate.metadata._metadata import BasisMetadata
+from slate.array import Array
+from slate.basis import DiagonalBasis
+from slate.metadata import BasisMetadata
 
 if TYPE_CHECKING:
-    from slate.basis._basis import Basis
-    from slate.metadata.stacked import Metadata2D
+    from slate.basis import Basis
+    from slate.metadata import Metadata2D
 
 
 def extract_diagonal[M: BasisMetadata, E, DT: np.generic](
@@ -21,3 +22,11 @@ def extract_diagonal[M: BasisMetadata, E, DT: np.generic](
     converted = array.with_basis(b)
 
     return Array(converted.basis.inner[1], converted.raw_data)
+
+
+def abs[M: BasisMetadata, DT: np.generic](  # noqa: A001
+    array: Array[BasisMetadata, DT],
+) -> Array[BasisMetadata, np.float64]:
+    """Calculate the absolute value of the array."""
+    converted = _array.as_index_basis(array)
+    return Array(converted.basis, np.abs(converted.raw_data))
