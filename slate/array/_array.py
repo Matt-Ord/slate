13d8dfaeb8a51aabdffff18ecb9bc31cd866fb75
Matthew Ord <matthew.ord1@gmail.com>
2024-12-10 11:19:33 +0000

    Type improvements, add iter to array

--- slate/array/_array.py
+++ slate/array/_array.py
@@ -1,15 +1,24 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Any, cast
+from typing import TYPE_CHECKING, Any, Never, cast, overload
 
 import numpy as np
 
 from slate import basis
 from slate.basis import Basis, TupleBasis
-from slate.metadata import BasisMetadata, NestedLength, shallow_shape_from_nested
+from slate.metadata import (
+    BasisMetadata,
+    Metadata2D,
+    NestedLength,
+    shallow_shape_from_nested,
+)
 
 if TYPE_CHECKING:
+    from collections.abc import Iterator
+
+    from slate.basis._tuple import TupleBasis2D
     from slate.metadata import SimpleMetadata, StackedMetadata
+    from slate.metadata.stacked import Metadata1D, Metadata3D
 
 
 class Array[
@@ -58,11 +67,9 @@ class Array[
 
     def as_array(self) -> np.ndarray[Any, np.dtype[DT]]:
         """Get the data as a (full) np.array."""
-        metadata = self.basis.metadata()
-        fundamental = basis.from_metadata(metadata, is_dual=self.basis.is_dual)
+        fundamental = basis.as_fundamental(self.basis)
         shape = shallow_shape_from_nested(fundamental.fundamental_shape)
-        converted = self.basis.__convert_vector_into__(self._data, fundamental)
-        return converted.reshape(shape)
+        return self.with_basis(fundamental).raw_data.reshape(shape)
 
     @staticmethod
     def from_array[DT1: np.generic](
@@ -112,3 +119,44 @@ class Array[
         as_mul_basis = basis.as_mul_basis(self.basis)
         data = as_mul_basis.mul_data(self.with_basis(as_mul_basis).raw_data, other)
         return Array(as_mul_basis, data)
+
+    @overload
+    def __iter__[_DT: np.generic](
+        self: Array[Metadata1D[Any, Any], _DT, Basis[Any, Any]], /
+    ) -> Never: ...
+    @overload
+    def __iter__[_M1: BasisMetadata, _DT: np.generic](
+        self: Array[Metadata2D[Any, _M1, Any], _DT, Basis[Any, Any]], /
+    ) -> Iterator[Array[Basis[_M1, Any], _DT]]: ...
+    @overload
+    def __iter__[_M1: BasisMetadata, _M2: BasisMetadata, _DT: np.generic](
+        self: Array[Metadata3D[Any, _M1, _M2, Any], _DT, Basis[Any, Any]], /
+    ) -> Iterator[
+        Array[TupleBasis2D[Any, Basis[_M1, Any], Basis[_M2, Any], Any], _DT]
+    ]: ...
+    @overload
+    def __iter__[_M1: BasisMetadata, _DT: np.generic](
+        self: Array[StackedMetadata[_M1, Any], _DT, Basis[_M1, Any]], /
+    ) -> Iterator[Array[StackedMetadata[_M1, None], _DT]]: ...
+
+    def __iter__(
+        self: Array[Any, Any, Basis[BasisMetadata, Any]], /
+    ) -> Iterator[Array[Any, Any]]:
+        basis_as_tuple = basis.with_modified_child(
+            basis.as_tuple_basis(self.basis), basis.as_fundamental, 0
+        )
+        as_tuple = self.with_basis(basis_as_tuple)
+        children = as_tuple.basis.children[1:]
+        match len(children):
+            case 0:
+                msg = "Cannot iterate over a flat array."
+                raise ValueError(msg)
+            case 1:
+                out_basis = children[0]
+            case _:
+                out_basis = basis.tuple_basis(children)
+
+        return (
+            Array(out_basis, row)
+            for row in as_tuple.raw_data.reshape(as_tuple.basis.shape)
+        )
